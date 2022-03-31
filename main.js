d475ad884e847f400efd5ced13b2ae14b30b5d1a
Chehak-Goyal <102141173+Chehak-Goyal@users.noreply.github.com>
2022-03-31 13:06:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,8 +11,41 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 380, 380);
+    if(Status != "")
+    {
+      objectDetector.detect(video, gotResult);
+      for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "status : Objects Detected ";
+          fill("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+          if(objects[i].label==object_name){
+              video.stop();
+              objectDetector.detect(gotResult);
+              document.getElementById("object_status").innerHTML= object_name + "Found";
+              synth= window.speechSynthesis;
+              utter= new speechSynthesisisUtterance(object_name + "Found");
+              synth.speak(utter);
+          }
+          else{
+              document.getElementById("object_status").innerHTML= object_name + "Not Found";
+          }
+
+      }  
+    }
 }
 
+function gotResult(error, results)
+{
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
